4da13e891b43ed8384d20497f43c3251c1da6eef
Isamuna Nkembo Josue <josamuna2009@gmail.com>
2026-06-21 14:06:40 +0200

    Step 7 of GreetingBot Workshop Project: Creating two variables with let and log message to the console

---
+++ Workshop/1-GreetingBot/index.js
@@ -0,0 +1,8 @@
+console.log("Hi there!");
+console.log("I am excited to talk to you.");
+let bot;
+bot = "teacherBot";
+
+let botLocation = "the universe";
+
+console.log("Allow me to introduce myself.");
