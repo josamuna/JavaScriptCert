57ef18e724bbc03b8ee8cba19a119fc3e149aa4c
Isamuna Nkembo Josue <josamuna2009@gmail.com>
2026-06-22 19:30:34 +0200

    Step 6 of TeacherChatbot Workshop Project: Using template literals and String interpolation

---
+++ Workshop/2-TeacherChatbot/index.js
@@ -0,0 +1,12 @@
+console.log("Hi there!");
+
+const botName = "teacherBot";
+
+const greeting = `My name is ${botName}.`;
+console.log(greeting);
+
+const subject = "JavaScript";
+const topic = "strings";
+
+const sentence = `Today, you will learn about ${topic} in ${subject}.`;
+console.log(sentence);
