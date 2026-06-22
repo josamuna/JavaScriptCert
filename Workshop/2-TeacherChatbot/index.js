5b141ca01d71c635c2cd289f1400cabf0b88bafa
Isamuna Nkembo Josue <josamuna2009@gmail.com>
2026-06-22 19:58:55 +0200

    Completed TeacherChatbot Workshop Project with the use of indexOf method

--- Workshop/2-TeacherChatbot/index.js
+++ Workshop/2-TeacherChatbot/index.js
@@ -38,3 +38,16 @@ console.log(
 
 const lastCharacter = subject[subject.length - 1];
 console.log(lastCharacter);
+
+const learningIsFunSentence = "Learning is fun.";
+
+console.log(
+  "Here are examples of finding the positions of substrings in the sentence.",
+);
+
+console.log(learningIsFunSentence.indexOf("Learning"));
+
+console.log(learningIsFunSentence.indexOf("fun"));
+console.log(learningIsFunSentence.indexOf("learning"));
+
+console.log("I hope you enjoyed learning today.");
