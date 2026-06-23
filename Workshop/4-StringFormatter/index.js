849819e663886e29c68587cc255ec3c472dce5e0
Isamuna Nkembo Josue <josamuna2009@gmail.com>
2026-06-24 01:17:42 +0200

    Step 16 of StringFormatter Workshop Project: Formatting a String in camelCase mode using combined function slice() and toUpperCase()

--- Workshop/4-StringFormatter/index.js
+++ Workshop/4-StringFormatter/index.js
@@ -21,3 +21,11 @@ console.log(upperCaseInput);
 const lowerCaseInput = cleanedInput.toLowerCase();
 console.log("Result of using the toLowerCase() method:");
 console.log(lowerCaseInput);
+
+const lowercaseWord = "camelcase";
+const camelCasedVersion =
+  lowercaseWord.slice(0, 5) +
+  lowercaseWord[5].toUpperCase() +
+  lowercaseWord.slice(-3);
+console.log("Camel cased version:");
+console.log(camelCasedVersion);
