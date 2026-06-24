656d2f59de1cb6cd08e4d18775618375dc5dbf03
Isamuna Nkembo Josue <josamuna2009@gmail.com>
2026-06-24 02:17:51 +0200

    Step 7 of StringTransformer Workshop Project: Using replaceAll() function instead of replace() to change occurence of a substring inside another string

--- Workshop/5- StringTransformer/index.js
+++ Workshop/5- StringTransformer/index.js
@@ -5,3 +5,11 @@ console.log(originalString);
 const replacedString = originalString.replace("cats", "dogs");
 console.log("After using the replace() method:");
 console.log(replacedString);
+
+const exampleSentence = "I love cats and cats are so much fun!";
+console.log("Original sentence:");
+console.log(exampleSentence);
+
+const dogsOnlySentence = exampleSentence.replaceAll("cats", "dogs");
+console.log("Replacing all occurrences of cats with dogs:");
+console.log(dogsOnlySentence);
