97d5051a87b46aa53e08c6434d85536dd08a0f73
Isamuna Nkembo Josue <josamuna2009@gmail.com>
2026-06-26 02:26:12 +0200

    Completed FortuneTeller Lab Project

---
+++ Labs/5-FortuneTeller/index.js
@@ -0,0 +1,23 @@
+const fortune1 = "Your cat will look very cuddly today.";
+const fortune2 = "The weather will be nice tomorrow.";
+const fortune3 = "Be cautious of your new neighbors.";
+const fortune4 = "You will find a new hobby soon.";
+const fortune5 = "It would be wise to avoid the color red today.";
+let selectedFortune = "";
+const min = 1;
+const max = 6;
+const randomNumber = Math.floor(Math.random() * (max - min)) + min;
+
+if (randomNumber === 1) {
+  selectedFortune = fortune1;
+} else if (randomNumber === 2) {
+  selectedFortune = fortune2;
+} else if (randomNumber === 3) {
+  selectedFortune = fortune3;
+} else if (randomNumber === 4) {
+  selectedFortune = fortune4;
+} else if (randomNumber === 5) {
+  selectedFortune = fortune5;
+}
+
+console.log(selectedFortune);
