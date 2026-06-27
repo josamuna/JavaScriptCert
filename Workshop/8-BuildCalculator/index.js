340033b4ccad963f51c4963f2d9e42a31d52ae9c
Isamuna Nkembo Josue <josamuna2009@gmail.com>
2026-06-27 21:20:04 +0200

    Last Step of Calculator Workshop Project: Adding Square and Square root calculation

--- Workshop/8-BuildCalculator/index.js
+++ Workshop/8-BuildCalculator/index.js
@@ -26,3 +26,17 @@ function calculateQuotient(num1, num2) {
 
 console.log(calculateQuotient(7, 11));
 console.log(calculateQuotient(3, 0));
+
+function calculateSquare(num) {
+  return num ** 2;
+}
+
+console.log(calculateSquare(2));
+console.log(calculateSquare(9));
+
+function calculateSquareRoot(num) {
+  return Math.sqrt(num);
+}
+
+console.log(calculateSquareRoot(25));
+console.log(calculateSquareRoot(100));
