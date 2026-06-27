cb46cabb0fe30cd19256a3454ec9125c1564ac1e
Isamuna Nkembo Josue <josamuna2009@gmail.com>
2026-06-27 21:15:03 +0200

    Step 15 of Calculator Workshop Project: Proper handling division by zero to avoid get Infinity

--- Workshop/8-BuildCalculator/index.js
+++ Workshop/8-BuildCalculator/index.js
@@ -21,7 +21,8 @@ function calculateProduct(num1, num2) {
 console.log(calculateProduct(13, 5));
 
 function calculateQuotient(num1, num2) {
-  return num1 / num2;
+  return num2 === 0 ? "Error: Division by zero" : num1 / num2;
 }
 
 console.log(calculateQuotient(7, 11));
+console.log(calculateQuotient(3, 0));
