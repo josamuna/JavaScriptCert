499512ff16f058ba7ed65db83f632797845b7ab6
Isamuna Nkembo Josue <josamuna2009@gmail.com>
2026-06-27 21:05:33 +0200

    Step 9 of Calculator Workshop Project: Performing sum of two numbers for differents values

--- Workshop/8-BuildCalculator/index.js
+++ Workshop/8-BuildCalculator/index.js
@@ -1,11 +1,7 @@
-function addTwoAndSeven() {
-  return 2 + 7;
+function calculateSum(num1, num2) {
+  return num1 + num2;
 }
 
-console.log(addTwoAndSeven());
-
-function addThreeAndFour() {
-  return 3 + 4;
-}
-
-console.log(addThreeAndFour());
+console.log(calculateSum(2, 5));
+console.log(calculateSum(10, 10));
+console.log(calculateSum(5, 5));
