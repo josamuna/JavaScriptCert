e810333d7f8b8828776c10dbfe1826e52dcdb2f1
Isamuna Nkembo Josue <josamuna2009@gmail.com>
2026-06-28 10:22:16 +0200

    Passing CelsiusFahrenheitConverter Lab Project

---
+++ Labs/8-CelsiusFahrenheitConverter/index.js
@@ -0,0 +1,32 @@
+function convertCtoF(celsiusTemp) {
+  if (Number.isNaN(celsiusTemp)) {
+    return;
+  }
+  // F = C * (9/5) + 32.
+
+  return celsiusTemp * (9 / 5) + 32;
+}
+
+// Test with invalid inputs.
+let celsius = Number.NaN;
+console.log(convertCtoF(celsius)); // undefined
+celsius = 0 / 0;
+console.log(convertCtoF(celsius)); // undefined
+celsius = undefined;
+console.log(convertCtoF(celsius)); // undefined
+celsius = null;
+console.log(convertCtoF(celsius)); // NaN
+celsius = "value";
+console.log(convertCtoF(celsius)); // NaN
+
+// Test with valid inputs.
+celsius = 0;
+console.log(convertCtoF(celsius)); // 32 F
+celsius = -30;
+console.log(convertCtoF(celsius)); // -22 F
+celsius = -10;
+console.log(convertCtoF(celsius)); // 14 F
+celsius = 20;
+console.log(convertCtoF(celsius)); // 68 F
+celsius = 30;
+console.log(convertCtoF(celsius)); // 86 F
