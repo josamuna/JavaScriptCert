07bea18ff5503d5c233ea2190e920a1d460e590c
Isamuna Nkembo Josue <josamuna2009@gmail.com>
2026-06-28 02:36:18 +0200

    Passing the EmailMasker Lab Project

---
+++ Labs/7-EmailMasker/index.js
@@ -0,0 +1,25 @@
+function maskEmail(email) {
+  if (!email) {
+    return "Invalid email address.";
+  }
+  let maskedEmail = "";
+  const atIndex = email.indexOf("@");
+  maskedEmail += email[0];
+  const partOfEmailToReplace = email.slice(1, atIndex - 1);
+  const maskedEmailText = "*".repeat(partOfEmailToReplace.length);
+  const remainedEmail = email.slice(atIndex - 1);
+  maskedEmail += maskedEmailText + remainedEmail;
+
+  return maskedEmail;
+}
+
+let email = "mymaskedemail@email.com";
+console.log(maskEmail(email));
+email = "apple.pie@example.com";
+console.log(maskEmail(email));
+email = "freecodecamp@example.com";
+console.log(maskEmail(email));
+email = "info@test.dev";
+console.log(maskEmail(email));
+email = "user@domain.org";
+console.log(maskEmail(email));
