c899c5d399a35f6808ec8039a6ae0e56948e3a4b
Isamuna Nkembo Josue <josamuna2009@gmail.com>
2026-06-29 23:37:18 +0200

    Step 10 of ShoppingList Workshop Project: Adding item to the beginning of the array using unshift() function

--- Workshop/10-ShoppingList/index.js
+++ Workshop/10-ShoppingList/index.js
@@ -11,3 +11,12 @@ function getShoppingListMsg(arr) {
 }
 
 console.log(getShoppingListMsg(shoppingList));
+
+shoppingList.push("Grapes");
+console.log(getShoppingListMsg(shoppingList));
+
+console.log("It looks like we need to get some cooking oil.");
+
+shoppingList.unshift("Vegetable Oil");
+
+console.log(getShoppingListMsg(shoppingList));
