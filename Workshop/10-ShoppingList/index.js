366f9e7d320b9c5b93c4a6c1b8c1c63d752cf091
Isamuna Nkembo Josue <josamuna2009@gmail.com>
2026-06-29 23:28:29 +0200

    Step 6 of ShoppingList Workshop Project: View the array content using a separate function

--- Workshop/10-ShoppingList/index.js
+++ Workshop/10-ShoppingList/index.js
@@ -5,3 +5,9 @@ const shoppingList = [];
 console.log("It will be nice to have some fruit to eat.");
 
 shoppingList.push("Apples");
+
+function getShoppingListMsg(arr) {
+  return `Current Shopping List: ${arr}`;
+}
+
+console.log(getShoppingListMsg(shoppingList));
