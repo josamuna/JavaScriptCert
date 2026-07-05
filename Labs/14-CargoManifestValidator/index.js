832fc0a69b93902967d40e7ec6962f91275c889c
Isamuna Nkembo Josue <josamuna2009@gmail.com>
2026-07-05 11:45:51 +0200

    Updating normalizeUnits function for a proper output and to pass the requirements

--- Labs/14-CargoManifestValidator/index.js
+++ Labs/14-CargoManifestValidator/index.js
@@ -1,32 +1,23 @@
 function normalizeUnits(manifest) {
-  let formatedManifest = {};
   if (Object.hasOwn(manifest, "unit") && Object.hasOwn(manifest, "weight")) {
     if (manifest.unit === "lb") {
       if (
         typeof manifest.weight !== "number" ||
         Number.isNaN(manifest.weight)
       ) {
-        return formatedManifest;
+        return {};
       }
 
-      // Convert lb to kg : 1lb = 0.45 kg
-      const lbWeight = manifest.weight;
-      const kgWeight = parseFloat(lbWeight * 0.45);
-      formatedManifest.weight = kgWeight;
-
-      // Updating the object by referenced the new one to return
-      formatedManifest.containerId = manifest.containerId;
-      formatedManifest.destination = manifest.destination;
-      formatedManifest.unit = "kg";
-      formatedManifest.hazmat = manifest.hazmat;
-
-      return formatedManifest;
+      return {
+        containerId: manifest.containerId,
+        destination: manifest.destination,
+        weight: manifest.weight * 0.45,
+        unit: "kg",
+        hazmat: manifest.hazmat,
+      };
     }
-
-    formatedManifest = manifest;
   }
-
-  return formatedManifest;
+  return manifest;
 }
 
 function validateManifest(manifest) {}
