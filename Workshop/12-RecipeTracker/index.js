e32203ec3c71729f2bf00fa580fcd19b0cc6c8e6
Josue Isamuna Nkembo <josamuna2009gmail.com>
2026-07-08 15:16:28 +0200

    Step 3 of RecipeTracker Workshop Project: Creating recipe array and object

---
+++ Workshop/12-RecipeTracker/index.js
@@ -0,0 +1,9 @@
+const recipes = [];
+
+const recipe1 = {
+  name: "Spaghetti Carbonara",
+  ingredients: ["spaghetti", "Parmesan cheese", "pancetta", "black pepper"],
+  cookingTime: 22,
+  totalIngredients: null,
+  difficultyLevel: "",
+};
