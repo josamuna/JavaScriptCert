d2291f549f7e8d7b9630cc95b1470f3ee044dbb2
Josue Isamuna Nkembo <josamuna2009gmail.com>
2026-07-16 13:49:35 +0200

    Step 6 of SentenceAnalyzer Workshop Project: Building a Ponctuation Count function

--- Workshop/14-SentenceAnalyzer/index.js
+++ Workshop/14-SentenceAnalyzer/index.js
@@ -31,3 +31,18 @@ function getConsonantCount(sentence) {
 
 const consonantCount = getConsonantCount("Coding is fun");
 console.log(`Consonant Count: ${consonantCount}`);
+
+function getPunctuationCount(sentence) {
+  const punctuations = ".,!?;:-()[]{}\"'–";
+  let count = 0;
+
+  for (const char of sentence) {
+    if (punctuations.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
+
+let punctuationCount = getPunctuationCount("WHAT?!?!?!?!?");
+console.log(`Punctuation Count: ${punctuationCount}`);
