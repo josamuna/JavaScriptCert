37005e009db21b00d99b000edd4d224259177971
Isamuna Nkembo Josue <josamuna2009@gmail.com>
2026-07-17 00:08:01 +0200

    Adding runSequence but without fully passed the code

--- Labs/17-TrafficLightSequencer/index.js
+++ Labs/17-TrafficLightSequencer/index.js
@@ -29,3 +29,54 @@ const config4 = {
   fault: false,
   phases: [],
 };
+
+function runSequence(config, cycles) {
+  for (const conf in config) {
+    if (conf === "phases") {
+      if (config[conf].length === 0) {
+        console.log("No phases found");
+        return;
+      } else {
+        for (let i = 0; i < cycles; i++) {
+          for (let j = 0; j < config[conf].length; j++) {
+            if (config[conf][j].duration <= 0) {
+              console.log("Invalid phase detected");
+              continue;
+            } else {
+              console.log(
+                `Switching to ${config[conf][j].color} for ${config[conf][j].duration} s`,
+              );
+            }
+          }
+        }
+      }
+    } else {
+      if (config[conf]) {
+        console.log("Faulted phase!");
+        break;
+      }
+    }
+  }
+}
+
+function generateTimeline(config, cycles) {}
+
+console.log("-----------1-----------------");
+
+runSequence(config1, 1);
+
+console.log("------------2----------------");
+
+runSequence(config1, 2);
+
+console.log("-------------3---------------");
+
+runSequence(config2, 1);
+
+console.log("-------------4---------------");
+
+runSequence(config3, 2);
+
+console.log("-------------5---------------");
+
+runSequence(config4, 5);
