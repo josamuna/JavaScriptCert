8cbdf24e0a6e17e5feabea4c964db0e78b07128b
Josue Isamuna Nkembo <josamuna2009gmail.com>
2026-07-16 12:05:17 +0200

    Step 9 of WordCounter Workshop Project: start building the getMatchedWordCount function

--- Workshop/13-WordCounter/index.js
+++ Workshop/13-WordCounter/index.js
@@ -4,3 +4,22 @@ function printCharacters(str) {
   }
 }
 printCharacters("hello");
+
+function getMatchedWordCount(sentence, match) {
+  let count = 0;
+
+  for (const word of sentence) {
+    console.log(
+      `Checking "${word}" against "${match}" | Running count: ${count}`,
+    );
+  }
+
+  return count;
+}
+
+console.log(
+  getMatchedWordCount(
+    ["I", "really", "really", "really", "like", "to", "code"],
+    "really",
+  ),
+);
