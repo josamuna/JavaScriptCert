d0581f13ba58054b20a3f55a8e144397aba24cfa
Josue Isamuna Nkembo <josamuna2009gmail.com>
2026-07-16 13:42:22 +0200

    Switching to the workshop14 branch to avoid working on the main branch

---
+++ Workshop/14-SentenceAnalyzer/index.js
@@ -0,0 +1,33 @@
+/*
+    The sentence analyzer will take a sentence and get the count for the number of words, vowels, consonants, and punctuation marks
+ */
+
+function getVowelCount(sentence) {
+  const vowels = "aeiou";
+  let count = 0;
+
+  for (const char of sentence.toLowerCase()) {
+    if (vowels.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
+
+const vowelCount = getVowelCount("Apples are tasty fruits");
+console.log(`Vowel Count: ${vowelCount}`);
+
+function getConsonantCount(sentence) {
+  const consonants = "bcdfghjklmnpqrstvwxyz";
+  let count = 0;
+
+  for (const char of sentence.toLowerCase()) {
+    if (consonants.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
+
+const consonantCount = getConsonantCount("Coding is fun");
+console.log(`Consonant Count: ${consonantCount}`);
