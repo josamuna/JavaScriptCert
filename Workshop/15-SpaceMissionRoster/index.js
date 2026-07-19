132f43a46aef5718ace56fe942093b3235ca8436
Isamuna Nkembo Josue <josamuna2009@gmail.com>
2026-07-19 18:41:54 +0200

    Step 14 of SpaceMissionRoster Workshop Project: Permorming swap from and array using slice() and splice() functions without mutating the original array

--- Workshop/15-SpaceMissionRoster/index.js
+++ Workshop/15-SpaceMissionRoster/index.js
@@ -82,4 +82,16 @@ function swapCrewMembers(crew, fromIndex, toIndex) {
   }
 
   const updatedCrew = crew.slice();
+  updatedCrew[fromIndex] = updatedCrew.splice(
+    toIndex,
+    1,
+    updatedCrew[fromIndex],
+  )[0];
+  for (let i = 0; i < updatedCrew.length; i++) {
+    console.log(updatedCrew[i].name);
+  }
+
+  return updatedCrew;
 }
+
+const updatedSquad = swapCrewMembers(squad, 2, 5);
