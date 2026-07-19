f09358c735301afa64d4458c48cacce939377a38
Isamuna Nkembo Josue <josamuna2009@gmail.com>
2026-07-19 20:29:27 +0200

    Completed SpaceMissionRoster Workshop Project

--- Workshop/15-SpaceMissionRoster/index.js
+++ Workshop/15-SpaceMissionRoster/index.js
@@ -135,8 +135,9 @@ const EVAChunks = chunkCrew(EVAReadySquad, 3);
 function printCrewSummary(crew) {
   const sorted = crew.slice();
   sortByPriorityDescending(sorted);
-
-  for (let i = 0; i < sorted.length; i++) {
-    console.log(sorted[i].name);
+  for (const astronaut of sorted) {
+    console.log(astronaut.name);
   }
 }
+
+printCrewSummary(updatedSquad);
