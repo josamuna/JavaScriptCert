cb1658a9d7305de46a6e459fb56df273d021718f
Isamuna Nkembo Josue <josamuna2009@gmail.com>
2026-07-19 16:55:48 +0200

    Step 3 of SpaceMissionRoster Workshop Project: Creating Empty array and Astronaut Object

---
+++ Workshop/15-SpaceMissionRoster/index.js
@@ -0,0 +1,13 @@
+/*
+    This workshop is about leveraging JavaScript to build a roster of astronauts for a space mission.
+*/
+
+const squad = [];
+
+const firstAstronaut = {
+  id: 1,
+  name: "Andy",
+  role: "Commander",
+  isEVAEligible: true,
+  priority: 3,
+};
