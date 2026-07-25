dc870304d85843c3a65a1d4308d451eb73a971a7
Josue Isamuna Nkembo <josamuna2009gmail.com>
2026-07-25 12:26:06 +0200

    Building the lab project structure

---
+++ Labs/24-MissingLetterDetector/index.js
@@ -0,0 +1,21 @@
+/*
+  This lab is about to find the missing letter in an input string.
+*/
+
+function fearNotLetter(str) {
+  if (!str || typeof str !== "string") {
+    return "Invalid input entry";
+  }
+
+  let missingLetter;
+
+  return missingLetter;
+}
+
+// Invalid input.
+let outputValue = "";
+console.log(outputValue);
+
+// Valid input.
+outputValue = fearNotLetter("abce");
+console.log(outputValue);
