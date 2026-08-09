d05f3526cab21ba7dd8971cb774190c278485049
Isamuna Nkembo Josue <josamuna2009@gmail.com>
2026-08-09 20:09:56 +0200

    Adding the titleCase() function without passing the challenge

--- Labs/34-TitleCaseConverter/README.md
+++ Labs/34-TitleCaseConverter/README.md
@@ -6,7 +6,7 @@ Build a Title Case Converter Project.
 
 ```js
 /*
-    This lab is about building
+    This lab is about creating a function that converts a string to title case. Title case means that the first letter of each word is capitalized and the rest of the word is in lower case.
 */
 ```
 
---
+++ Labs/34-TitleCaseConverter/index.js
@@ -0,0 +1,40 @@
+/*
+    This lab is about creating a function that converts a string to title case. Title case means that the first letter of each word is capitalized and the rest of the word is in lower case.
+*/
+
+function titleCase(strPhrase) {
+  if (!strPhrase) {
+    return;
+  }
+
+  let titleCase = "";
+
+  const chuncks = strPhrase.split(" ");
+
+  for (let index = 0; index < chuncks.length; index++) {
+    if ((index + 1) % chuncks.length !== 0) {
+      // Not last index.
+      titleCase +=
+        chuncks[index][0].toUpperCase() + chuncks[index].slice(1) + " ";
+    } else {
+      // Last index
+      titleCase += chuncks[index][0].toUpperCase() + chuncks[index].slice(1);
+    }
+  }
+
+  return titleCase;
+}
+
+//Invalid inputs.
+let title = titleCase(null);
+console.log(title); // ndefined
+
+title = titleCase("");
+console.log(title); // ndefined
+
+// Valid inputs.
+title = titleCase("I like to code");
+console.log(title); // I Like To Code
+
+title = titleCase("javaScript is fun");
+console.log(title); // JavaScript Is Fun
