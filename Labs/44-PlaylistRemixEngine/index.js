5a323bbfe8c14d6f58d7f1e851596a10c19ca739
Isamuna Nkembo Josue <josamuna2009@gmail.com>
2026-08-22 22:23:25 +0200

    Pass flattenPlaylists() function

--- Labs/44-PlaylistRemixEngine/index.js
+++ Labs/44-PlaylistRemixEngine/index.js
@@ -1,6 +1,15 @@
 /*
     This lab is about building a program that creates a single remix playlist 
     from multiple playlists submitted by listeners. 
+
+    Each listener provides a list of songs they want to hear. 
+    Some songs may appear more than once, and some artists may show up too many times. 
+    Your job is to work through these playlists step by step: 
+    1. Combine them into one list, 
+    2. Score each song, 
+    3. Remove duplicate songs, 
+    4. Limit how often the same artist appears, and then 
+    5. create a final play order.
 */
 
 const playlists = [
@@ -44,3 +53,30 @@ const playlists = [
     },
   ],
 ];
+
+function flattenPlaylists(arrayPlaylists) {
+  if (!Array.isArray(arrayPlaylists) || arrayPlaylists.lenght === 0) {
+    return [];
+  }
+
+  let flattenPlaylists = [];
+
+  for (let i = 0; i < arrayPlaylists.length; i++) {
+    for (let j = 0; j < arrayPlaylists[i].length; j++) {
+      const { trackId, artist, title, votes, bpm } = arrayPlaylists[i][j];
+      flattenPlaylists.push({
+        trackId: trackId,
+        artist: artist,
+        title: title,
+        votes: votes,
+        bpm: bpm,
+        source: [i, j],
+      });
+    }
+  }
+
+  return flattenPlaylists;
+}
+
+let flattenItem = flattenPlaylists(playlists);
+console.log(flattenItem);
