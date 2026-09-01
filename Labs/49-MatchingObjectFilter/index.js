604eb713fdf2049916c333c9b0b2c21caac0984e
Isamuna Nkembo Josue <josamuna2009@gmail.com>
2026-09-02 01:21:53 +0200

    Progressing with MatchingObjectFilter Lab Project with issues

--- Labs/49-MatchingObjectFilter/index.js
+++ Labs/49-MatchingObjectFilter/index.js
@@ -20,3 +20,72 @@
   // should return [{ first: "Tybalt", last: "Capulet" }]
 
 */
+
+function whatIsInAName(names, sourceObj) {
+  if (!Array.isArray(names) || names.length === 0) {
+    return;
+  }
+
+  if (Object.keys(sourceObj).length === 0) {
+    return;
+  }
+
+  const keys = Object.keys(sourceObj);
+  let arrayNames = [];
+  keys.forEach((key) => {
+    arrayNames = names.filter((name) => {
+      return Object.hasOwn(name, key) && name[key] === sourceObj[key];
+    });
+  });
+
+  return arrayNames;
+}
+
+let outputResult = whatIsInAName(
+  [
+    { first: "Romeo", last: "Montague" },
+    { first: "Mercutio", last: null },
+    { first: "Tybalt", last: "Capulet" },
+  ],
+  { last: "Capulet" },
+);
+console.log(outputResult); // [{ first: "Tybalt", last: "Capulet" }]
+
+outputResult = whatIsInAName(
+  [{ apple: 1 }, { apple: 1 }, { apple: 1, bat: 2 }],
+  { apple: 1 },
+);
+console.log(outputResult); // [{"apple": 1}, {"apple": 1}, {"apple": 1, "bat": 2}]
+
+outputResult = whatIsInAName(
+  [{ apple: 1, bat: 2 }, { bat: 2 }, { apple: 1, bat: 2, cookie: 2 }],
+  { apple: 1, bat: 2 },
+);
+console.log(outputResult); // [{"apple": 1, "bat": 2}, {"apple": 1, "bat": 2, "cookie": 2}]
+
+outputResult = whatIsInAName(
+  [{ apple: 1, bat: 2 }, { apple: 1 }, { apple: 1, bat: 2, cookie: 2 }],
+  { apple: 1, cookie: 2 },
+);
+console.log(outputResult); // [{"apple": 1, "bat": 2, "cookie": 2}]
+
+outputResult = whatIsInAName(
+  [
+    { apple: 1, bat: 2 },
+    { apple: 1 },
+    { apple: 1, bat: 2, cookie: 2 },
+    { bat: 2 },
+  ],
+  { apple: 1, bat: 2 },
+);
+console.log(outputResult); // [{"apple": 1, "bat": 2}, {"apple": 1, "bat": 2, "cookie": 2}]====
+
+outputResult = whatIsInAName([{ a: 1, b: 2, c: 3 }], { a: 1, b: 9999, c: 3 }); //
+console.log(outputResult); //
+
+outputResult = whatIsInAName([{ a: 1, b: 2, c: 3, d: 9999 }], {
+  a: 1,
+  b: 9999,
+  c: 3,
+}); //
+console.log(outputResult); //
