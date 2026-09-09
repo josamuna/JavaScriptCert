535fc5fa376a672b190f58b30fbd34b543987d78
Isamuna Nkembo Josue <josamuna2009@gmail.com>
2026-09-09 14:50:14 +0200

    Progressing with the RangeBasedLCMCalculator Lab Project with an issue: Overloop detected from the test environment

--- Labs/50-RangeBasedLCMCalculator/index.js
+++ Labs/50-RangeBasedLCMCalculator/index.js
@@ -17,5 +17,84 @@ function smallestCommons(numbers) {
     return;
   }
 
-  //numbers.filter(num => )
+  // create an array from values between the treshold.
+  const newNumbers = [];
+  if (numbers[0] > numbers[1]) {
+    for (let i = numbers[1]; i <= numbers[0]; i++) {
+      newNumbers.push(i);
+    }
+  } else {
+    for (let i = numbers[0]; i <= numbers[1]; i++) {
+      newNumbers.push(i);
+    }
+  }
+
+  let count = 1;
+
+  while (true) {
+    const multiples = newNumbers.map((num) => {
+      return count % num === 0 ? count : num;
+    });
+    console.log(multiples);
+    const matched = multiples.every((numValue) => {
+      return numValue === count;
+    });
+
+    //console.log("------>", count, matched);
+
+    if (matched) {
+      return count;
+    }
+    count++;
+  }
 }
+
+let smallestCommonsValue = smallestCommons([23, 18]);
+console.log(smallestCommonsValue); // 60
+
+// ===================================================================
+
+// THIS FUNCTION WORKS ONLY FOR THE TWO VALUES PASSED AS PARAMETERS
+
+/* function smallestCommons(numbers) {
+  if (!Array.isArray(numbers) || numbers.length === 0) {
+    return;
+  }
+
+  if (numbers.length > 2) {
+    return;
+  }
+
+  const multiples1 = [],
+    multiples2 = [];
+  let count = 1;
+
+  while (true) {
+    multiples1.push(numbers[0] * count);
+    multiples2.push(numbers[1] * count);
+    count++;
+
+    const lcm = multiples1.filter((num) => {
+      return multiples2.includes(num);
+    });
+
+    if (lcm.length !== 0) {
+      return lcm[0];
+    }
+  }
+}
+
+let smallestCommonsValue = smallestCommons([1, 5]);
+console.log(smallestCommonsValue); // 5
+
+smallestCommonsValue = smallestCommons([5, 1]);
+console.log(smallestCommonsValue); // 5
+
+smallestCommonsValue = smallestCommons([2, 10]);
+console.log(smallestCommonsValue); // 10
+
+smallestCommonsValue = smallestCommons([1, 13]);
+console.log(smallestCommonsValue); // 13
+
+smallestCommonsValue = smallestCommons([23, 18]);
+console.log(smallestCommonsValue); // 414 */
